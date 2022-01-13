3f6dc4e8f78842ee663bda9f09546b6d68653763
Jeanine Schoessler <jeanine.mt@gmail.com>
2022-01-12 18:00:20 -0700

    fix product tabs

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,2 +1,32 @@
 // Things we need to add can go here :)
-console.log('Things we need to add can go here :)')
+console.log('Things we need to add can go here :)');
+
+// (function ($) {
+//   $('.resp-tab-item').click(function(){
+//     console.log('mew');
+//   });
+
+// })(jQuery);
+
+(function ($) {
+  // $('.resp-tab-content').hide();
+  // $('.resp-tab-content:first-of-type').show();
+  // $('.resp-tab-item:first-of-type').addClass('resp-tab-active')
+  $('.resp-tab-item').click(function(){
+    var tabId = $(this).attr('aria-controls')
+    switchActiveTab(tabId);
+  });
+
+  switchActiveTab('tab_item-0')
+
+  function switchActiveTab(tabId) {
+    console.log('mew', tabId);
+
+
+    $('.resp-tab-item').removeClass('resp-tab-active')
+    $('.resp-tab-content').hide();
+    $('.resp-tab-content#' + tabId).show();
+    $('.resp-tab-item#li-' + tabId).addClass('resp-tab-active')
+    
+  }
+})(jQuery);
